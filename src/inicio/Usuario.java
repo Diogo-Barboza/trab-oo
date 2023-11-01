d69d47b2213f251bc1eb9cb9b186c77dfa5ec94f
diogo-barboza <diogorodriguesbb@gmail.com>
2023-11-01 16:22:52 -0300

    atualizacao Usuario

--- src/inicio/Usuario.java
+++ src/inicio/Usuario.java
@@ -1,5 +1,25 @@
 package inicio;
 
 public abstract class Usuario {
-
+	protected String nome;
+	protected String email;
+	protected String senha;
+	
+	public Usuario(String nome, String email, String senha) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	public void cadastrarNome(String n) {
+		nome = n;
+	}
+	public void cadastrarEmail(String e) {
+		email = e;
+	}
+	public void cadastrarSenha(String s) {
+		senha = s;
+	}
+	
 }
